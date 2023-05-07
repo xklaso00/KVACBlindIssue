f937a7c683e0d4f48774398dae03345295d409ca
xklaso00 <xklaso00@vutbr.cz>
2023-05-07 17:09:11 +0200

    test

--- MOKtry/types.hpp
+++ MOKtry/types.hpp
@@ -1,8 +1,8 @@
-#include <uECC_Parameters_t.hpp>
-#include <uECC_List_t.hpp>
-#include <uECC_vli.h>
+#include "uECC_Parameters_t.hpp"
+#include "uECC_List_t.hpp"
+#include "uECC_vli.h"
 //#include <Crypto.h>
-#include <SHA256.h>
+#include "SHA256.h"
 //#include <Arduino.h>
 
 #define HASH_SIZE 32
